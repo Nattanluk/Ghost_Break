2100283c5e60434b629f51f7b82e65ec837b5f9d
Nattanluk <nataniellucena01@gmail.com>
2026-05-18 15:01:48 -0300

    Game e Pulo
    Desenvolvi o arquivo game.py, e comecei a base da movimentação e da interface

--- Saulo_prota.py
+++ Saulo_prota.py
@@ -1,6 +1,9 @@
 # Saulo_prota.py
 import pygame
 
+LARGURA_TELA = 800
+CHAO_Y = 300
+
 
 class Saulo:
 
@@ -10,32 +13,47 @@ class Saulo:
         self.largura = 40
         self.altura = 60
         self.velocidade = 5
-        self.vel_y = 0
+        self.vel_y = 1
         self.f_gravidade = 0.6
         self.forca_pulo = -12
         self.no_chao = False
 
     def mover(self, teclas):
-        if teclas[pygame.K_a]:
+        tempo_pulo = 0
+        if teclas[pygame.K_LEFT]:
             self.pos_x -= self.velocidade
 
-        if teclas[pygame.K_d]:
+        if teclas[pygame.K_RIGHT]:
             self.pos_x += self.velocidade
 
-        if teclas[pygame.K_SPACE] and self.no_chao:
-            self.vel_y = self.forca_pulo
-            self.no_chao = False
+        if teclas[pygame.K_UP] and self.no_chao:
+            while tempo_pulo < 15:
+                self.pos_y += self.forca_pulo
+                self.no_chao = False
+                tempo_pulo += 1
+            self.pos_y -= self.f_gravidade
+
+            
+
+
+        if self.pos_x < 0:
+            self.pos_x = 0
+
+        if self.pos_x + self.largura > LARGURA_TELA:
+            self.pos_x = LARGURA_TELA - self.largura
 
     def gravidade(self):
+
         self.vel_y += self.f_gravidade
         self.pos_y += self.vel_y
 
-        if self.pos_y >= 300:
-            self.pos_y = 300
+        if self.pos_y >= CHAO_Y:
+            self.pos_y = CHAO_Y
             self.vel_y = 0
             self.no_chao = True
 
     def update(self, teclas):
+
         self.mover(teclas)
         self.gravidade()
 
--- game.py
+++ game.py
@@ -1,8 +1,42 @@
-#game.py
-from Saulo_prota import Saulo
 import pygame
+from Saulo_prota import Saulo
+
+LARGURA = 800
+ALTURA = 400
+
 
 class Jogo:
 
     def __init__(self):
-       pass
+
+        pygame.init()
+        self.tela = pygame.display.set_mode((LARGURA, ALTURA))
+        pygame.display.set_caption("Ghost_Break")
+        self.clock = pygame.time.Clock()
+        self.player = Saulo(100, 300)
+
+    def iniciar(self):
+
+        rodando = True
+        while rodando:
+
+            self.clock.tick(60)
+            for evento in pygame.event.get():
+
+                if evento.type == pygame.QUIT:
+                    rodando = False
+
+            teclas = pygame.key.get_pressed()
+            self.player.update(teclas)
+            self.tela.fill((30, 30, 30))
+
+            pygame.draw.rect(
+                self.tela,
+                (100, 255, 100),
+                (0, 360, 800, 40)
+            )
+
+            self.player.desenhar(self.tela)
+            pygame.display.update()
+
+        pygame.quit()
