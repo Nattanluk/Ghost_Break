a146f4d1c404b4f42dcd27e577fe412fefb04522
Nattanluk <nataniellucena01@gmail.com>
2026-09-02 10:02:19 -0300

    chao

--- Mundo/plataformas.py
+++ Mundo/plataformas.py
@@ -9,27 +9,13 @@ class Plataforma:
         self.rect = pygame.Rect(x, y, largura, altura)
 
         # Área de colisão
-        self.rect_colisao = pygame.Rect(
-            x - 15,
-            y,
-            largura + 15,
-            altura
-        )
+        self.rect_colisao = pygame.Rect(x - 15, y, largura + 30, altura)
 
         # Sprite da plataforma
-        self.imagem = pygame.transform.scale(
-            imagem,
-            (largura, altura)
-        )
+        self.imagem = pygame.transform.scale(imagem, (largura, altura))
 
     def desenhar(self, tela, camera_x):
-        tela.blit(
-            self.imagem,
-            (
-                self.rect.x - camera_x,
-                self.rect.y
-            )
-        )
+        tela.blit(self.imagem, (self.rect.x - camera_x, self.rect.y))
 
 
 class Criar_Plataforma:
@@ -37,6 +23,7 @@ class Criar_Plataforma:
     def __init__(self):
         # Carrega a sprite uma única vez
         self.sprite_plataforma = pygame.image.load("imagens/1000328238.png").convert_alpha()
+        self.sprite_chao = pygame.image.load("imagens/chao.png").convert_alpha()
 
     def criar(self):
 
@@ -44,60 +31,29 @@ class Criar_Plataforma:
 
         # Chão - primeiro trecho
         for x in range(0, 30):
-            plataformas.append(Plataforma(x * 20 , 410, 20, 40,self.sprite_plataforma))
+            plataformas.append(Plataforma(x * 20, 410, 50, 60, self.sprite_chao))
 
         # Segundo trecho
         for x in range(38, 70):
-            plataformas.append(
-                Plataforma(
-                    x * 20,
-                    410,
-                    20,
-                    40,
-                    self.sprite_plataforma
-                )
-            )
+            plataformas.append(Plataforma(x * 20, 410, 50, 60, self.sprite_chao))
 
         # Terceiro trecho
         for x in range(80, 130):
-            plataformas.append(
-                Plataforma(
-                    x * 20,
-                    410,
-                    20,
-                    40,
-                    self.sprite_plataforma
-                )
-            )
+            plataformas.append(Plataforma(x * 20, 410, 50, 60, self.sprite_chao))
 
         # Quarto trecho
         for x in range(140, 180):
-            plataformas.append(
-                Plataforma(
-                    x * 20,
-                    410,
-                    20,
-                    40,
-                    self.sprite_plataforma
-                ))
+            plataformas.append(Plataforma(x * 20, 410, 50, 60, self.sprite_chao))
 
         # Plataformas suspensas
         plataformas.append(Plataforma(300, 305, 130, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(520, 280, 130, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(800, 230, 180, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(1150, 250, 150, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(1450, 320, 140, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(1750, 290, 160, 50, self.sprite_plataforma))
-
         plataformas.append(Plataforma(2000, 200, 440, 50, self.sprite_plataforma))
-
-        plataformas.append(Plataforma(2190, 105, 40, 32, self.sprite_plataforma))
-
+        plataformas.append(Plataforma(2190, 110, 100, 50, self.sprite_plataforma))
         plataformas.append(Plataforma(2650, 280, 100, 50, self.sprite_plataforma))
 
         return plataformas
